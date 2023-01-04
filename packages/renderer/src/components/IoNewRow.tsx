6d0e08fa16daab9b64554637045f2df743688a70
Marc Rother <monestereo@gmail.com>
2023-01-04 21:50:20 +0100

    hands refactor

--- packages/renderer/src/components/IoNewRow.tsx
+++ packages/renderer/src/components/IoNewRow.tsx
@@ -1,55 +1,61 @@
-import { Button, Stack } from '@mui/material'
-import produce from 'immer'
-import { useMemo, useState } from 'react'
-import { v4 as uuidv4 } from 'uuid'
-import { InputSelector } from '@/components/InputSelector'
-import { OutputSelector } from '@/components/OutputSelector'
-import type { Row } from '@/store/mainStore'
-import { useMainStore } from '@/store/mainStore'
+import { Button, Stack } from "@mui/material";
+import produce from "immer";
+import { useMemo, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { InputSelector } from "@/components/InputSelector";
+import { OutputSelector } from "@/components/OutputSelector";
+import type { Row } from "@/store/mainStore";
+import { useMainStore } from "@/store/mainStore";
 
 export const IoNewRow = ({ onComplete }: { onComplete: () => void }) => {
-  const addRow = useMainStore((state) => state.addRow)
-  const [templateRow, setRow] = useState<Partial<Row> & Pick<Row, 'id'>>({
+  const addRow = useMainStore((state) => state.addRow);
+  const [templateRow, setRow] = useState<Partial<Row> & Pick<Row, "id">>({
     id: uuidv4(),
-  })
+  });
   // console.log(templateRow)
-  const modules = useMainStore((state) => state.modules)
+  const modules = useMainStore((state) => state.modules);
   const selectedInputModule = useMemo(() => {
     if (!templateRow.input || !templateRow.inputModule) {
-      return undefined
+      return undefined;
     }
-    return modules[templateRow.inputModule]
-  }, [modules, templateRow])
+    return modules[templateRow.inputModule];
+  }, [modules, templateRow]);
 
   const selectedOutputModule = useMemo(() => {
     if (!templateRow.output || !templateRow.outputModule) {
-      return undefined
+      return undefined;
     }
-    return modules[templateRow.outputModule]
-  }, [modules, templateRow])
+    return modules[templateRow.outputModule];
+  }, [modules, templateRow]);
 
   const SelectedModuleInputEdit = useMemo(() => {
-    return selectedInputModule?.InputEdit
-  }, [selectedInputModule])
+    return selectedInputModule?.InputEdit;
+  }, [selectedInputModule]);
 
   const SelectedModuleOutputEdit = useMemo(() => {
-    return selectedOutputModule?.OutputEdit
-  }, [selectedOutputModule])
+    return selectedOutputModule?.OutputEdit;
+  }, [selectedOutputModule]);
 
   return (
     <>
       <Stack
-        direction={'row'}
+        direction={"row"}
         sx={{
-          borderTop: '1px solid #bbb',
-          width: '100%',
-          justifyContent: 'space-between',
+          borderTop: "1px solid #bbb",
+          width: "100%",
+          justifyContent: "space-between",
           mt: 2,
           pt: 2,
           pb: 2,
         }}
       >
-        <div style={{ flexBasis: '50%', borderRight: '1px dashed #666', textAlign: 'left' }}>
+        <div
+          style={{
+            flexBasis: "50%",
+            borderRight: "1px dashed #666",
+            textAlign: "left",
+          }}
+        >
           <InputSelector
             onSelect={(modId, inp) => {
               setRow((row) => {
@@ -60,21 +66,26 @@ export const IoNewRow = ({ onComplete }: { onComplete: () => void }) => {
                     data: {},
                   },
                   inputModule: modId,
-                }
-              })
+                };
+              });
             }}
           />
           {templateRow.input && SelectedModuleInputEdit ? (
             <SelectedModuleInputEdit
               input={templateRow.input}
               onChange={(data: Record<string, any>) => {
-                setRow(
-                  produce((row) => {
-                    if (row.input) {
-                      Object.assign(row.input?.data, data)
-                    }
-                  })
-                )
+                setRow((row) => {
+                  return {
+                    ...row,
+                    input: {
+                      ...row.input,
+                      data: {
+                        ...(row.input?.data ?? {}),
+                        ...data,
+                      },
+                    },
+                  } as Partial<Row> & Pick<Row, "id">;
+                });
               }}
             ></SelectedModuleInputEdit>
           ) : (
@@ -82,7 +93,7 @@ export const IoNewRow = ({ onComplete }: { onComplete: () => void }) => {
           )}
         </div>
         <div
-          style={{ flexBasis: '50%', marginLeft: '10px', textAlign: 'left' }}
+          style={{ flexBasis: "50%", marginLeft: "10px", textAlign: "left" }}
         >
           <>
             <OutputSelector
@@ -95,8 +106,8 @@ export const IoNewRow = ({ onComplete }: { onComplete: () => void }) => {
                       data: {},
                     },
                     outputModule: modId,
-                  }
-                })
+                  };
+                });
               }}
             />
             {templateRow.output && SelectedModuleOutputEdit && (
@@ -106,10 +117,10 @@ export const IoNewRow = ({ onComplete }: { onComplete: () => void }) => {
                   setRow(
                     produce((row) => {
                       if (row.output) {
-                        Object.assign(row.output?.data, data)
+                        Object.assign(row.output?.data, data);
                       }
                     })
-                  )
+                  );
                 }}
               ></SelectedModuleOutputEdit>
             )}
@@ -117,8 +128,8 @@ export const IoNewRow = ({ onComplete }: { onComplete: () => void }) => {
         </div>
       </Stack>
       <Button
-        variant='contained'
-        size='small'
+        variant="contained"
+        size="small"
         sx={{ mr: 2 }}
         disabled={
           !templateRow.input ||
@@ -139,13 +150,13 @@ export const IoNewRow = ({ onComplete }: { onComplete: () => void }) => {
               inputModule: templateRow.inputModule,
               output: templateRow.output,
               outputModule: templateRow.outputModule,
-            })
-            onComplete()
+            });
+            onComplete();
           }
         }}
       >
         save
       </Button>
     </>
-  )
-}
+  );
+};
--- packages/renderer/src/modules/Mediapipe/Hands.tsx
+++ packages/renderer/src/modules/Mediapipe/Hands.tsx
@@ -1,209 +1,112 @@
-import Shortkey from '@/modules/Keyboard/Shortkey'
-import type { ModuleConfig, InputData, Row } from '@/store/mainStore'
-import { camelToSnake } from '@/utils'
-import { Button, Icon } from '@mui/material'
-import { FC, useEffect } from 'react'
-import Hands from '@mediapipe/hands'
-import Holistic from '@mediapipe/holistic'
+import Shortkey from "@/modules/Keyboard/Shortkey";
+import type { ModuleConfig, InputData, Row } from "@/store/mainStore";
+import { camelToSnake } from "@/utils";
+import { Button, Icon } from "@mui/material";
+import { FC, useEffect } from "react";
 import {
   detectGesture,
   Gesture,
-} from '../../modules/Mediapipe/Old/core/gesture-detector'
-import { VideoScene } from './Old/video/video-scene'
-import { useStore } from '@/store/OLD/useStore'
-import useRequestAnimationFrame from 'use-request-animation-frame'
-import { HandsEstimator } from './Old/core/hands-estimator'
+} from "../../modules/Mediapipe/Old/core/gesture-detector";
+import { HandsEstimator } from "./Old/core/hands-estimator";
+import create from "zustand";
 
-type HandsConfigExample = {}
+type HandsConfigExample = {};
 
-export const id = 'hands-module'
+export const id = "hands-module";
 
 export const moduleConfig: ModuleConfig<HandsConfigExample> = {
-  menuLabel: 'A.I.',
+  menuLabel: "A.I.",
   inputs: [
     {
-      name: 'Hands',
-      icon: 'sign_language',
+      name: "Hands",
+      icon: "sign_language",
     },
   ],
   outputs: [],
   config: {
     enabled: true,
   },
-}
+};
+
+type GestureState = {
+  gesture: Gesture;
+  setGesture: (gesture: Gesture) => void;
+};
+
+const useGestureStore = create<GestureState>()((set, get) => {
+  const estimator = new HandsEstimator();
+  let lastGesture: Gesture = Gesture.Unknown;
+  let i = 0;
+  estimator.addListener((results) => {
+    const landmarks = results?.multiHandLandmarks[0];
+
+    if (landmarks) {
+      const gesture = detectGesture(landmarks);
+      if (gesture === lastGesture) {
+        i++;
+        if (i === 10) {
+          get().setGesture(gesture);
+        }
+      } else {
+        lastGesture = gesture;
+        i = 0;
+      }
+    }
+  });
+  estimator.start();
+  return {
+    gesture: Gesture.Unknown,
+    setGesture: (gesture: Gesture) => {
+      set((state) => ({
+        ...state,
+        gesture,
+      }));
+    },
+  };
+});
 
 export const InputEdit: FC<{
-  input: InputData
-  onChange: (data: Record<string, any>) => void
+  input: InputData;
+  onChange: (data: Record<string, any>) => void;
 }> = ({ input, onChange }) => {
-  const cam = useStore((state) => state.inputs.cam)
-  //   const videoCanvas = useStore((state) => state.videoCanvas)
-  //   const videoScene = useStore((state) => state.videoScene)
-
-  const videoCanvas = document.getElementById(
-    'video-canvas'
-  ) as HTMLCanvasElement
-  const videoScene = new VideoScene(videoCanvas)
-
-  var i: number = 0
-  var currentGesture: Gesture | null = null
-  var results: Hands.Results | Holistic.Results | null = null
-  var hand: Hands.LandmarkList | Holistic.LandmarkList | null = null
+  const gesture = useGestureStore((state) => state.gesture);
   useEffect(() => {
-    const listener = (r: any) => {
-      results = r
-      const landmarks = r?.multiHandLandmarks[0]
-      if (landmarks) {
-        hand = landmarks
-        const gesture = detectGesture(landmarks)
-        if (gesture === currentGesture) {
-          i++
-          if (i === 10) {
-            onChange({
-              data: {
-                value: Gesture[gesture],
-              },
-            })
-          }
-        } else {
-          currentGesture = gesture
-          i = 0
-        }
-      }
+    if (Gesture[gesture] !== input.data?.value) {
+      onChange({
+        value: Gesture[gesture],
+      });
     }
+  }, [gesture]);
 
-    const handsEstimator = new HandsEstimator()
-
-    if (videoCanvas) {
-      if (cam) {
-        handsEstimator.addListener(listener)
-        handsEstimator.start()
-        videoCanvas.style.display = 'block'
-      } else {
-        handsEstimator.stop()
-        handsEstimator.removeListener(listener)
-        videoCanvas.style.display = 'none'
-      }
-    }
-
-    return () => {
-      handsEstimator.stop()
-      handsEstimator.removeListener(listener)
-      if (videoCanvas) {
-        videoCanvas.style.display = 'none'
-      }
-    }
-  }, [cam])
-
-  useRequestAnimationFrame(
-    (e: any) => {
-      if (results && videoScene) videoScene.update(results as any)
-    },
-    { duration: undefined, shouldAnimate: cam }
-  )
   return (
-    <div style={{ textAlign: 'left', marginTop: '10px' }}>
-      <Button variant='outlined'>{input?.data?.data?.value || ''}</Button>
+    <div style={{ textAlign: "left", marginTop: "10px" }}>
+      <Button variant="outlined">{input.data?.value || ""}</Button>
     </div>
-  )
-}
+  );
+};
 
 export const InputDisplay: FC<{ input: InputData }> = ({ input }) => {
-  console.log('HERE', input)
+  console.log("HERE", input);
   return (
     <>
-      {' '}
+      {" "}
       <Icon>{camelToSnake(input.icon)}</Icon>
       <Shortkey
-        value={input.data.data.value}
+        value={input.data.value}
         trigger={() => {
-          console.log('SHORTKEY;')
+          console.log("SHORTKEY;");
         }}
       />
     </>
-  )
-}
+  );
+};
 
-export const useInputActions = (
-  row: Row
-  // onChange: (data: Record<string, any>) => void
-) => {
-  const cam = useStore((state) => state.inputs.cam)
-  const inMqtt = useStore((state) => state.inputs.mqtt)
+export const useInputActions = (row: Row) => {
+  const gesture = useGestureStore((state) => state.gesture);
 
-  //   const videoCanvas = useStore((state) => state.videoCanvas)
-  //   const videoScene = useStore((state) => state.videoScene)
-  const videoCanvas = document.getElementById(
-    'video-canvas'
-  ) as HTMLCanvasElement
-  const videoScene = new VideoScene(videoCanvas)
-  var i: number = 0
-  var currentGesture: Gesture | null = null
-  var results: Hands.Results | Holistic.Results | null = null
-  var hand: Hands.LandmarkList | Holistic.LandmarkList | null = null
-
-  console.log('WHY0', row)
   useEffect(() => {
-    console.log('WHY')
-    const listener = (r: any) => {
-      results = r
-      const landmarks = r?.multiHandLandmarks[0]
-      if (landmarks) {
-        hand = landmarks
-        const gesture = detectGesture(landmarks)
-        if (gesture === currentGesture) {
-          // onChange({ currentGesture })
-          i++
-          if (i === 10) {
-            window.dispatchEvent(
-              new CustomEvent(`io_input`, { detail: row.id })
-            )
-
-            // const check = shortcuts.find(
-            //     (s: any) =>
-            //         s.input_type === 'cam' &&
-            //         s.shortkey === Gesture[gesture].toLowerCase()
-            // )
-            // if (check) {
-            //     window.dispatchEvent(new CustomEvent(`io_input`, { detail: row.id }))
-            //     //   actions(check.output_type, check.action)
-            // } else {
-            //     setShortcut(Gesture[gesture].toLowerCase())
-            // }
-          }
-        } else {
-          currentGesture = gesture
-          i = 0
-        }
-      }
+    if (Gesture[gesture] === row.input.data.value) {
+      window.dispatchEvent(new CustomEvent(`io_input`, { detail: row.id }));
     }
-
-    const handsEstimator = new HandsEstimator()
-    if (videoCanvas) {
-      if (inMqtt) {
-        handsEstimator.addListener(listener)
-        handsEstimator.start()
-        videoCanvas.style.display = 'block'
-      } else {
-        handsEstimator.stop()
-        handsEstimator.removeListener(listener)
-        videoCanvas.style.display = 'none'
-      }
-    }
-
-    return () => {
-      handsEstimator.stop()
-      handsEstimator.removeListener(listener)
-      if (videoCanvas) {
-        videoCanvas.style.display = 'none'
-      }
-    }
-  }, [cam, inMqtt])
-
-  useRequestAnimationFrame(
-    (e: any) => {
-      if (results && videoScene) videoScene.update(results as any)
-    },
-    { duration: undefined, shouldAnimate: cam }
-  )
-}
+  }, [gesture]);
+};
