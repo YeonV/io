c4c646a553a456328b00757fe6ef163ba979ef0a
Yeon Vinzenz Varapragasam <YeonVinzenz.Varapragasam@trustedshops.de>
2023-01-12 10:57:59 +0100

    oops

--- packages/renderer/src/modules/Mediapipe/Hands.tsx
+++ packages/renderer/src/modules/Mediapipe/Hands.tsx
@@ -14,7 +14,7 @@ import { useStore } from '@/store/OLD/useStore'
 import useRequestAnimationFrame from 'use-request-animation-frame'
 import { HandsEstimator } from './Old/core/hands-estimator'
 import Settings from '@/components/OLD/Settings'
-import ModuleButton from '@/components/ModuleButton'
+import DisplayButtons from '@/components/DisplayButtons'
 
 type HandsConfigExample = {}
 
@@ -115,7 +115,7 @@ export const InputDisplay: FC<{ input: InputData }> = ({ input }) => {
   console.log('HERE', input)
   return (
     <>
-      <ModuleButton data={input} />
+      <DisplayButtons data={input} />
       <Shortkey
         value={input.data.data.value}
         trigger={() => {
@@ -127,76 +127,76 @@ export const InputDisplay: FC<{ input: InputData }> = ({ input }) => {
 }
 
 export const useCam = () =>
-  // row: Row
-  // onChange: (data: Record<string, any>) => void
-  {
-    const cam = useStore((state) => state.inputs.cam)
-    const inMqtt = useStore((state) => state.inputs.mqtt)
+// row: Row
+// onChange: (data: Record<string, any>) => void
+{
+  const cam = useStore((state) => state.inputs.cam)
+  const inMqtt = useStore((state) => state.inputs.mqtt)
 
-    //   const videoCanvas = useStore((state) => state.videoCanvas)
-    //   const videoScene = useStore((state) => state.videoScene)
-    const videoCanvas = document.getElementById(
-      'video-canvas'
-    ) as HTMLCanvasElement
-    const videoScene = new VideoScene(videoCanvas)
-    var i: number = 0
-    var currentGesture: Gesture | null = null
-    var results: Hands.Results | Holistic.Results | null = null
-    var hand: Hands.LandmarkList | Holistic.LandmarkList | null = null
+  //   const videoCanvas = useStore((state) => state.videoCanvas)
+  //   const videoScene = useStore((state) => state.videoScene)
+  const videoCanvas = document.getElementById(
+    'video-canvas'
+  ) as HTMLCanvasElement
+  const videoScene = new VideoScene(videoCanvas)
+  var i: number = 0
+  var currentGesture: Gesture | null = null
+  var results: Hands.Results | Holistic.Results | null = null
+  var hand: Hands.LandmarkList | Holistic.LandmarkList | null = null
 
-    // console.log('WHY0', row)
-    useEffect(() => {
-      console.log('WHY')
-      const listener = (r: any) => {
-        results = r
-        const landmarks = r?.multiHandLandmarks[0]
-        if (landmarks) {
-          hand = landmarks
-          const gesture = detectGesture(landmarks)
-          if (gesture === currentGesture) {
-            // onChange({ currentGesture })
-            i++
-            if (i === 10) {
-              // window.dispatchEvent(
-              //   new CustomEvent(`io_input`, { detail: row.id })
-              // )
+  // console.log('WHY0', row)
+  useEffect(() => {
+    console.log('WHY')
+    const listener = (r: any) => {
+      results = r
+      const landmarks = r?.multiHandLandmarks[0]
+      if (landmarks) {
+        hand = landmarks
+        const gesture = detectGesture(landmarks)
+        if (gesture === currentGesture) {
+          // onChange({ currentGesture })
+          i++
+          if (i === 10) {
+            // window.dispatchEvent(
+            //   new CustomEvent(`io_input`, { detail: row.id })
+            // )
 
-              console.log('Fire', gesture)
-            }
-          } else {
-            currentGesture = gesture
-            i = 0
+            console.log('Fire', gesture)
           }
-        }
-      }
-
-      const handsEstimator = new HandsEstimator()
-      if (videoCanvas) {
-        if (inMqtt) {
-          handsEstimator.addListener(listener)
-          handsEstimator.start()
-          videoCanvas.style.display = 'block'
         } else {
-          handsEstimator.stop()
-          handsEstimator.removeListener(listener)
-          videoCanvas.style.display = 'none'
+          currentGesture = gesture
+          i = 0
         }
       }
+    }
 
-      return () => {
+    const handsEstimator = new HandsEstimator()
+    if (videoCanvas) {
+      if (inMqtt) {
+        handsEstimator.addListener(listener)
+        handsEstimator.start()
+        videoCanvas.style.display = 'block'
+      } else {
         handsEstimator.stop()
         handsEstimator.removeListener(listener)
-        if (videoCanvas) {
-          videoCanvas.style.display = 'none'
-        }
+        videoCanvas.style.display = 'none'
       }
-    }, [cam, inMqtt])
+    }
 
-    useRequestAnimationFrame(
-      (e: any) => {
-        if (results && videoScene) videoScene.update(results as any)
-      },
-      { duration: undefined, shouldAnimate: cam }
-    )
-    // return cam ? <>yo</> : <>no</>
-  }
+    return () => {
+      handsEstimator.stop()
+      handsEstimator.removeListener(listener)
+      if (videoCanvas) {
+        videoCanvas.style.display = 'none'
+      }
+    }
+  }, [cam, inMqtt])
+
+  useRequestAnimationFrame(
+    (e: any) => {
+      if (results && videoScene) videoScene.update(results as any)
+    },
+    { duration: undefined, shouldAnimate: cam }
+  )
+  // return cam ? <>yo</> : <>no</>
+}
